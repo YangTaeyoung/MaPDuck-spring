ed84f015f7dc642dd17cc46d23a7b63108360844
subeen-park <mas_jaypark@naver.com>
2021-11-02 23:47:18 +0900

    Feat: 로그인 기능 추가

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,15 +1,16 @@
-const path = require("path");
+const path = require('path')
 module.exports = {
-    // vue로 인해 bulid될 때 출력되는 static파일들이 아래의 경로로 떨어지도록 설정
-    outputDir: path.resolve(__dirname, "../main/resources/static"),
-    // Vue Proxy 설정
-    devServer: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:9000',
-                ws: true,
-                changeOrigin: true
-            },
-        }
-    }
+  outputDir: path.resolve(__dirname, "../"+"main/resources/static"),
+  devServer:  {
+    proxy: {
+      '/api': {
+        target: 'http:/localhost:9000',
+        ws:true,
+        changeOrigin: true
+      },
+    },
+  },
+  transpileDependencies: [
+    'vuetify'
+  ]
 }
