d36a76a87eb8727fd3777ff0d85f1d21a877e5de
anfqlc1127@gmail.com <anfqlc1127@gmail.com>
2021-11-03 00:18:04 +0900

    feat: package 구조 재편성

--- src/main/java/com/mapduck/domain/Product.java
+++ src/main/java/com/mapduck/domain/Product.java
@@ -2,14 +2,28 @@ package com.mapduck.domain;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 public class Product {
 
-    private String title;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // pk를 db에서 알아서 해준다.
+    private Long id;
+
+
+    private String name;
+    private Long company;
+    private String modelName;
     private String description;
-    private String img_path;
+
+
 
 }
