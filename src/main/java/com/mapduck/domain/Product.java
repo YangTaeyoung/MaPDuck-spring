c0f2b24cb86ffaf8d3b7c140829842aa3f11d348
Taeyoung Yang <0130yang@gmail.com>
2021-11-07 05:24:25 +0900

    Fix: 실제 DB를 반영하도록 Entity 클래스 리펙토링

--- src/main/java/com/mapduck/domain/Product.java
+++ src/main/java/com/mapduck/domain/Product.java
@@ -2,15 +2,17 @@ package com.mapduck.domain;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 /**
  * 작성자: 강동연
  * 작성일: 2021.11.3
  * 설명: Product Entity
+ *
+ * 수정자: 양태영
+ * 수정일: 21.11.07
+ * 수정 내용: 실제 테이블 반영 후 매핑.
+ *
  */
 @Entity
 @Getter
@@ -18,19 +20,28 @@ import javax.persistence.Id;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "PRODUCT")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // pk를 db에서 알아서 해준다.
-    private Long id;
+    @Column(name = "PR_ID")
+    private Long prId;
 
 
-    private String name;
-    private Long companyId;
-    private String modelName;
+    @Column(name = "PR_NAME")
+    private String prName;
+
+    @ManyToOne
+    @JoinColumn(name = "PR_COMPANY")
+    private Company prCompany;
+
+    @Column(name = "MO_NAME")
+    private String moName;
+    @Column(name = "DESCRIPTION")
     private String description;
+    @Column(name = "IMG_PATH")
     private String imgPath;
-
 
 
 }
