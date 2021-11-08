be27778fcdf13f383fb59e93f9a17a5cdc2175e8
anfqlc1127@gmail.com <anfqlc1127@gmail.com>
2021-11-08 18:07:40 +0900

    remove: 임시 Repository와 관련된 파일 모두 제거

--- src/main/java/com/mapduck/repository/ProductRepository.java
+++ src/main/java/com/mapduck/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package com.mapduck.repository;
 
 import com.mapduck.domain.Product;
+import com.mapduck.dto.ProductDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findProductByPrNameContainsOrMoNameContainsOrPrCompany_CoName(String prName, String moName, String prCompany_coName);
 }
