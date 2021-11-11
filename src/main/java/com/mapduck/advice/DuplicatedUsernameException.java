570ebd1f8f331b21678367ae9c800dcbc0b1baa8
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-11 20:39:36 +0900

    Feat: 회원 가입 시 username(이메일)이 중복되었을 때의 예외 추가.

---
+++ src/main/java/com/mapduck/advice/DuplicatedUsernameException.java
@@ -0,0 +1,13 @@
+package com.mapduck.advice;
+
+public class DuplicatedUsernameException extends RuntimeException {
+    public DuplicatedUsernameException(String message, Throwable cause){
+        super(message, cause);
+    }
+    public DuplicatedUsernameException(String message){
+        super(message);
+    }
+    public DuplicatedUsernameException(Throwable cause){
+        super(cause);
+    }
+}
