deb75267822b006331d583d2add3e5a0f4016648
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-11 23:31:05 +0900

    Feat: 멤버 클래스에 이메일 속성 추가.

--- src/main/java/com/mapduck/domain/Member.java
+++ src/main/java/com/mapduck/domain/Member.java
@@ -1,25 +1,39 @@
 package com.mapduck.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "MEMBER")
 public class Member {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     Long id;
 
     @Column(name = "NAME")
     String name;
 
-    @Column(name = "PHONE")
+    @Column(name = "PHONE", unique = true)
     String phone;
+
+    @Column(name = "EMAIL", unique = true)
+    String email;
 
     @Column(name = "UPDATED_AT", columnDefinition = "datetime default now()")
     LocalDateTime updatedAt;
 
     @Column(name = "JOINED_AT", columnDefinition = "datetime default now()")
     LocalDateTime joinedAt;
+
+    @OneToMany
+    @JoinColumn(name = "OWNER")
+    List<Own> owns;
 }
