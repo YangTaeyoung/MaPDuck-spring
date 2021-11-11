94a0a0f6f3c94cf7b9549cc25828533a13ac23c1
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-11 20:46:19 +0900

    Feat: 권한 관련 엔티티, 레파지토리 클래스 추가

--- src/main/java/com/mapduck/domain/Role.java
+++ src/main/java/com/mapduck/domain/Role.java
@@ -1,18 +1,30 @@
 package com.mapduck.domain;
 
-import javax.persistence.*;
-import java.util.Collection;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 @Table(name = "ROLE")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ROLE_ID")
+    @Column(name = "ID")
     private Long roleId;
 
-    @Column(name = "ROLE_NAME")
+    @Column(name = "NAME")
     private String roleName;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name= "USER_ROLES",
+        joinColumns = @JoinColumn(name = "ROLE_ID", referencedColumnName = "ID"),
+        inverseJoinColumns = @JoinColumn(name= "USER_ID", referencedColumnName = "ID")
+    )
+    private List<User> users;
 
 }
