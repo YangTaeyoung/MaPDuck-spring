f88ff3462b2bab63a3e80b8f14d368488108fcaf
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-11 23:31:32 +0900

    Feat: 회원가입 관련 기능 추가.

--- src/main/java/com/mapduck/serivce/MemberService.java
+++ src/main/java/com/mapduck/serivce/MemberService.java
@@ -5,7 +5,6 @@ import com.mapduck.domain.Member;
 import java.util.List;
 
 public interface MemberService {
-    List<Member> getMembers(String keyword);
     Member getMember(Long id);
     void saveMember(Member member);
     void deleteMember(Long id);
