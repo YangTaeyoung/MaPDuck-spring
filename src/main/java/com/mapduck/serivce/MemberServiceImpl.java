3457fa1a4d2b969aed39d988ef28ba68bf0b880c
Taeyoung Yang <0130yang@gmail.com>
2021-11-15 03:19:56 +0900

    Comment: 주석 추가

--- src/main/java/com/mapduck/serivce/MemberServiceImpl.java
+++ src/main/java/com/mapduck/serivce/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package com.mapduck.serivce;
 
 import com.mapduck.domain.Member;
 import com.mapduck.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,13 @@ import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
+@RequiredArgsConstructor
 @Service
 public class MemberServiceImpl implements MemberService{
 
-    @Autowired
-    private MemberRepository memberRepository;
 
-    @Autowired
-    private RestTemplate restTemplate;
+    private final MemberRepository memberRepository;
+    private final  RestTemplate restTemplate;
 
     @Override
     public Member getMember(Long id) {
@@ -34,12 +34,24 @@ public class MemberServiceImpl implements MemberService{
         this.memberRepository.save(member);
     }
 
+    /**
+     * 작성자: 양태영
+     * 작성일: 21.11.15
+     * 설명: id를 기준으로 member를 삭제하는 함수
+     * @param id: 삭제할 member의 id값
+     */
     @Override
     public void deleteMember(Long id) {
         Member member = getMember(id);
         this.memberRepository.delete(member);
     }
 
+    /**
+     * 작성자: 양태영
+     * 작성일: 21.11.15
+     * @param user: UserDetails를 상속받는 Spring의 User객체 (우리가 구현한 것 아님)
+     * @return member: 이메일을 기준으로 조회한 member Entity 객체
+     */
     @Transactional
     @Override
     public Member metaUserToMember(User user) {
