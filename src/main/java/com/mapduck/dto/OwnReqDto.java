00c0b5f13a45970928491880a762241845f62721
Taeyoung Yang <0130yang@gmail.com>
2021-11-15 03:22:40 +0900

    Feat: 디폴트 값 추가. 및 Data어노테이션 오류로 인한 따로 추가.

--- src/main/java/com/mapduck/dto/OwnReqDto.java
+++ src/main/java/com/mapduck/dto/OwnReqDto.java
@@ -2,14 +2,18 @@ package com.mapduck.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Data;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OwnReqDto {
     Long ownerId;
     Long prId;
-    LocalDateTime purchaseAt;
+    LocalDateTime purchaseAt = LocalDateTime.now();
 }
