34d871f4344c32847e86fcec62dea73f80bed739
Taeyoung Yang <0130yang@gmail.com>
2021-11-15 00:56:15 +0900

    Fix: 의존성 주입을 위한 @Repository 어노테이션 추가.

--- src/main/java/com/mapduck/repository/CompanyRepository.java
+++ src/main/java/com/mapduck/repository/CompanyRepository.java
@@ -2,6 +2,8 @@ package com.mapduck.repository;
 
 import com.mapduck.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 }
--- src/main/java/com/mapduck/repository/RoleRepository.java
+++ src/main/java/com/mapduck/repository/RoleRepository.java
@@ -4,7 +4,9 @@ import com.mapduck.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query("from Role where roleName=:roleName")
     Role findByRoleName(@Param("roleName") String roleName);
