fd89321afadff9928de7c0a92bcde2874312882f
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-15 20:44:09 +0900

    Feat: 이메일 핸드폰 중복체크 컨트롤러 함수 추가.

--- src/main/java/com/mapduck/controller/UserController.java
+++ src/main/java/com/mapduck/controller/UserController.java
@@ -8,14 +8,18 @@ import com.mapduck.mapper.JoinMapper;
 import com.mapduck.serivce.MemberService;
 import com.mapduck.serivce.UserService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -23,14 +27,29 @@ public class UserController {
     private final JoinMapper joinMapper;
     private final MemberService memberService;
 
-
     @Transactional
-    @PostMapping("/user")
-    public ResponseEntity<UserVO> register(@RequestBody JoinFormDto joinFormDto) {
+    @PostMapping("")
+    public ResponseEntity register(@RequestBody JoinFormDto joinFormDto) {
         UserVO user = joinMapper.joinFormToUserVO(joinFormDto);
         Member member = joinMapper.joinFormToMember(joinFormDto);
         memberService.saveMember(member);
         userService.save(user);
-        return new ResponseEntity<UserVO>(user, HttpStatus.CREATED);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/chke")
+    public ResponseEntity<Map<String, Boolean>> checkEmail(@RequestParam("ea") String encodedEmail){
+        String decodedEmail = new String(Base64.decodeBase64(encodedEmail));
+        var validation = new HashMap<String, Boolean>();
+        validation.put("chk_value", memberService.checkEmail(decodedEmail));
+        return new ResponseEntity<>(validation, HttpStatus.OK);
+    }
+
+    @GetMapping("/chkp")
+    public ResponseEntity<Map<String, Boolean>> checkPhone(@RequestParam("pn") String encodedPhone){
+        String decodedPhone = new String(Base64.decodeBase64(encodedPhone));
+        var validation = new HashMap<String, Boolean>();
+        validation.put("chk_value", memberService.checkPhone(decodedPhone));
+        return new ResponseEntity<>(validation, HttpStatus.OK);
     }
 }
