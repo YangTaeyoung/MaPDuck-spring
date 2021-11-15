58dc0b306c3beacd5c4ef4aeb4b505052b9fffb5
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-15 20:38:22 +0900

    Feat: 핸드폰기반 검색 함수 추가.

--- src/main/java/com/mapduck/repository/MemberRepository.java
+++ src/main/java/com/mapduck/repository/MemberRepository.java
@@ -14,4 +14,5 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
+    Member findByPhone(String phone);
 }
