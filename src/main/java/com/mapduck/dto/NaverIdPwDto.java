4f1858ad01c853f55317c9285d21e72163ea564c
Taeyoung Yang <59782504+YangTaeyoung@users.noreply.github.com>
2021-11-17 13:22:27 +0900

    fix: JsonNamingStrategy 추가.

--- src/main/java/com/mapduck/dto/NaverIdPwDto.java
+++ src/main/java/com/mapduck/dto/NaverIdPwDto.java
@@ -1,10 +1,14 @@
 package com.mapduck.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
+
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class NaverIdPwDto {
 
     private String id;
--- src/main/java/com/mapduck/dto/OwnResDto.java
+++ src/main/java/com/mapduck/dto/OwnResDto.java
@@ -1,5 +1,7 @@
 package com.mapduck.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class OwnResDto {
     private Long ownId;
     private LocalDateTime purchaseAt;
